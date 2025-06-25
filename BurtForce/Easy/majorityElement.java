aaea37537fefd6abc2c1da08ea09d1892dc3d581
Nischay <nischaykumar2606@gmail.com>
2025-06-26 02:32:57 +0530

    Add files via upload

---
+++ BurtForce/Easy/majorityElement.java
@@ -0,0 +1,14 @@
+package BruteForce.BurteForce.Easy;
+
+public class majorityElement {
+    public int majorityElements(int[] nums){
+        for (int i = 0; i < nums.length; i++) {
+            int count = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] == nums[j]) count++;
+            }
+            if (count>nums.length/2) return nums[i];
+        }
+        return 0;
+    }
+}
